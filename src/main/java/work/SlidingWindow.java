bda19f5cc961c8adbf5aa3f4197db6fea48d4eda
yongtaek <skillove@gmail.com>
2021-02-01 23:25:01 +0900

    ..

--- src/main/java/work/SlidingWindow.java
+++ src/main/java/work/SlidingWindow.java
@@ -3,9 +3,7 @@ package work;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class SlidingWindow {
   private ConcurrentMap<Long, AtomicInteger> windows = new ConcurrentHashMap<>();
   // 시간 경계선 문제해결을 위한 마지막 요청 시간 정보
@@ -14,7 +12,7 @@ public class SlidingWindow {
   private final double minute = 60000.0;
   // 졔약 시간 Window
   private final int windowSizeInMs;
-  // windows 시간당 최대 요총 횟수
+  // windows 시간당 최대 요청 횟수
   private final int maxRequestPerWindow;
 
   /**
@@ -28,36 +26,33 @@ public class SlidingWindow {
 
   boolean allow() {
     long now = System.currentTimeMillis();
-    // windows 키값 설정
+    // window 키값 설정
     long curWindowKey = now / windowSizeInMs * windowSizeInMs;
-    // 현재 window에서의 요청 횟수 등록
+    // 현재 window 실행횟수 초기화
     windows.putIfAbsent(curWindowKey, new AtomicInteger(0));
-    // 60초 이전 키
+    // 지정된 제약시간에 해당하는 키값 설정
     long preWindowKey = curWindowKey - windowSizeInMs;
-    // 이전 윈도우크기에 대한 요청 횟수 가져오기
+    // 이전 window에 대한 요청 횟수 조회
     AtomicInteger preCount = windows.get(preWindowKey);
-//    INFO: now=1612179124154,curWindowKey=1612179120000, preWindowKey=1612179060000
-//    log.info("now={},curWindowKey={}, preWindowKey={}",now,curWindowKey,preWindowKey);
     if (preCount == null) {
-          lastMS = now;
-//      60초 이전에 데이터가 없으면 현재 갯수를 증가하고 값 비교 반환
+      // 마지막 실행시간 저장
+      lastMS = now;
+      //이전 window 데이터가 없으면 현재 갯수를 증가및 최대값 초과 여부 반환
       return windows.get(curWindowKey).incrementAndGet() <= maxRequestPerWindow;
     }
     long ago = now - lastMS;
-    // 60초동안 요청이 없는경우 모든 조건을 초기화한다.
+    // 지정된 제약시간 동안 요청이 없는경우 모든 조건을 초기화한다.
     if (ago >= windowSizeInMs) {
-      log.info("init!! ago={}, windowSizeInMs={}",ago,windowSizeInMs);
       lastMS = now;
       windows = new ConcurrentHashMap<>();
       return true;
     }
 
-    // ex) 75초에 요청이 발생한경우 60초와 120초의 0.25(25%) 지점 나머지 지점의 0.75(75%)를 계산한다.
+    // ex) 1분 15초에 요청이 발생한경우 1분과 2분의 0.25(25%) 지점에 대한 나머지 0.75(75%)를 계산한다.
     double preWeight = 1-(ago/minute);
     // 제한시간내 호출된 횟수 * 요청시간 나머지 지점 비율 + 제한시간 초과후 호출된 횟수
     long count = (long) (preCount.get() * preWeight + windows.get(curWindowKey).incrementAndGet());
-    log.info("### preCount={},preWeight={},curCount={}, ago={}, result={}",preCount.get(),preWeight,windows.get(curWindowKey).get(),ago,count <= maxRequestPerWindow);
-//    상태값 변경
+    // 마지막 실행시간 저장
     lastMS = now;
     // maxRequestPerWindow 보다 작으면 요청 처리, 그렇지 않으면 거부
     return count <= maxRequestPerWindow;
